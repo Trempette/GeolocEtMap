04826facbbcac328d886e319ad067ebf24ae64c9
Tom Noden <ntprogramation@gmail.com>
2018-10-30 21:40:50 +0100

    viewpager

--- app/src/main/java/fr/edouardkerhir/geolocmap/ListActivity.java
+++ app/src/main/java/fr/edouardkerhir/geolocmap/ListActivity.java
@@ -2,13 +2,23 @@ package fr.edouardkerhir.geolocmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView mListTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        getCandy();
+    }
 
+    public void getCandy() {
+
+        ListAdapter adapter = new ListAdapter(ListActivity.this, CandySingleton.getInstance().getCandyArrayList());
+        mListTrip = findViewById(R.id.candy_list);
+        mListTrip.setAdapter(adapter);
     }
 }
